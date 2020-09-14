92d44ebceec65118e01050af66ef9ca2818d7929
vybhav sreshta <srehtavybhav@gmail.com>
2020-09-14 09:33:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,9 +7,17 @@ class Assessment():
         driver=webdriver.Chrome(executable_path="C:\\Users\\Administrator\\Downloads\\chromedriver.exe")
         driver.get("http://jt-dev.azurewebsites.net/#/SignUp")
         time.sleep(5)
+        river.find_element_by_css_selector('div#language .pull-left.ui-select-match-text').click()
+        val=driver.find_elements_by_xpath("//ul[@class='ui-select-choices ui-select-choices-content ui-select-dropdown dropdown-menu ng-scope']/li[@class='ui-select-choices-group']/div/a/div[@class='ng-binding ng-scope']")
+        ls=set()
+        for temp in val:
+            ls.add(temp.text)
+        if ls and list_of_languages:
+            print("Both Languages are present")
+        driver.find_elements_by_xpath("//ul[@class='ui-select-choices ui-select-choices-content ui-select-dropdown dropdown-menu ng-scope']/li[@class='ui-select-choices-group']/div/a/div[@class='ng-binding ng-scope']")[0].click()
         driver.find_element_by_id("name").send_keys("vybhav")
         driver.find_element_by_id("orgName").send_keys("vybhav")
-        driver.find_element_by_id("singUpEmail").send_keys("aazxc48@gmail.com")
+        driver.find_element_by_id("singUpEmail").send_keys("sreshtavybhav@gmail.com@gmail.com")
         driver.find_element_by_css_selector(".ui-checkbox>.black-color.ng-binding").click()
         driver.find_element_by_css_selector(".btn.btn-block.btn-custom.ng-binding").click()
         driver.close()
